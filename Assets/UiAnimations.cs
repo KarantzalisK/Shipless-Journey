b6d314cfbac47590112ea7dfb6966a021a0c86c3
Nick Bitounis <nikosmp1995@hotmail.com>
2019-01-27 01:56:45 +0200

    Changes graphics added

--- Assets/UiAnimations.cs
+++ Assets/UiAnimations.cs
@@ -6,6 +6,7 @@ public class UiAnimations : MonoBehaviour
 {
     public Animator CrewUiAnimator;
     public Animator DistanceUiAnimator;
+    public Animator PauseButtonUiAnimator;
     public void CrewUiAnimationTrigger()
     {
         CrewUiAnimator.Play("CrewUiAnimationLeft");
@@ -16,5 +17,9 @@ public class UiAnimations : MonoBehaviour
         DistanceUiAnimator.Play("MapDistanceComeTop");
     }
 
+    public void PauseUiComeIfFirstTime()
+    {
+        PauseButtonUiAnimator.Play("PauseUiAnimation");
+    }
 
 }
