5423200275cc667dedf18ed8ac5958dab20799e2
Athanasios Nektarios Karachalios Stagkas <nasos.karachalios.stagas@gmail.com>
2019-01-26 17:01:44 +0200

    half Made Biscuit(slider
    half Made Biscuit

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -4,24 +4,34 @@ using UnityEngine;
 using UnityEngine.UI;
 public class UIScript: MonoBehaviour
 {
-    float progressValue;
+    float progressValue,score;
     public GameObject player,destination;
     public Slider progressSlider;
+    public Text showScore;
     // Start is called before the first frame update
     void Start()
     {
         progressSlider.minValue = player.transform.position.x;
         progressSlider.maxValue = destination.transform.position.x;
+        progressValue = (destination.transform.position.x);
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        progressValue = (player.transform.position.x);
-        progressSlider.value = progressValue;
+        progressSlider.value =progressValue-destination.transform.position.x;
 
     }
+    private void OnTriggerEnter2D(Collider2D coli)
+    { if (coli.gameObject.tag == "Enemy")
+        {
+            coli.gameObject.SetActive(false);
+            score++;
+            showScore.text = "Score:" + score.ToString();
 
-   
+        }
+        
+    }
 }
