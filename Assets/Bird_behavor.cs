d5564071c1c1210ef4420d66f09a00abe627fac5
Vasileios Mourikis <mourogr@gmail.com>
2019-01-26 18:31:34 +0200

    sound bird and barell

---
+++ Assets/Bird_behavor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird_behavor : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("hit bird");
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
