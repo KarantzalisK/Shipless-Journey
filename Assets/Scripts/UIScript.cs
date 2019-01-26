fae6e64abaf7bc472b33fb925541045d3ee5c20b
Athanasios Nektarios Karachalios Stagkas <nasos.karachalios.stagas@gmail.com>
2019-01-26 14:58:18 +0200

    SLIDER
    slider track

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UIScript: MonoBehaviour
+{
+    float progressValue;
+    public GameObject player,destination;
+    public Slider progressSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        progressSlider.minValue = player.transform.position.x;
+        progressSlider.maxValue = destination.transform.position.x;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progressValue = (player.transform.position.x);
+        progressSlider.value = progressValue;
+
+    }
+
+   
+}
